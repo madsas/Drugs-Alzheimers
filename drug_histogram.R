510ece48c097fcbf5821698bcf0e65db3462ec95
U-Sasidhar-HP\Sasidhar <Sasidhar@Sasidhar-HP.(none)>
2014-12-03 08:05:32 -0800

    added unique drug uses

--- drug_histogram.R
+++ drug_histogram.R
@@ -1,37 +1,56 @@
-#read the data in 
+#read the data in#################### 
 if (!exists('data')) {
 	data <- read.csv("altmann06122013.csv", as.is=T)
 	data.df <- data.frame(data)
 }
-
 #Get drug columns
 dc <- grep("DRUG",colnames(data))
+#Get NACCID column
+n <- grep("NACCID",colnames(data))
 
-#Take all drugs, regardless of visits and make histogram
-all.drugs <- data[,dc]
-all.drugs.l <- as.list(all.drugs)
-#all.drugs.l.flat <- as.list(unlist(all.drugs.l))
-all.drugs.l.flat <- unlist(all.drugs.l)
-#remove empty entries (takes a lot of time for reason)
-all.drugs.l.flat.clean <- all.drugs.l.flat[as.logical(lapply(x,function(x) x!=""))]
-#make frequency table
-all.drugs.table <- table(all.drugs.l.flat.clean)
-#print most and least common
-tail(sort(all.drugs.table))
-head(sort(all.drugs.table))
+#SWITCH####################
+#switch <- 0
+switch <- 1
 
-if (FALSE) {
-#make one row per subject
-##get row number of first instance of each id
-ids <- unique(data$NACCID)
-rows <- c()
-for (i in ids) {
-	rows <- c(rows,which(data$NACCID==i)[1])
+if (!switch) {
+	#Take all drugs, regardless of visits and make histogram####################
+	all.drugs <- data[,dc]
+	all.drugs.l <- as.list(all.drugs)
+	all.drugs.l.flat <- unlist(all.drugs.l)
+	#remove empty entries 
+	all.drugs.l.flat.clean <- all.drugs.l.flat[as.logical(lapply(x,function(x) x!=""))]
+	#make frequency table
+	all.drugs.table <- sort(table(all.drugs.l.flat.clean))
+	#print most and least common
+	tail(all.drugs.table)
+	head(all.drugs.table)
+	#output to csv
+	write.table(all.drugs.table,file="sorted_all_drugs_table.csv",sep=",",col.names=NA,qmethod="double")
 }
 
-##make dataframe with just drugs and first instance of id
-
-
-#get drug column numbers and put in new df
-dc <- grep("DRUG",colnames(data))
+if (switch) {
+	#Now repeat but count subjects on drugs insteado of simply drug occurence####################
+	id.drugs <- data[,c(n,dc)]
+	#get list of ids and make empty data frame
+	ids<-unique(id.drugs$NACCID)
+	unique.drugs <- rep(list(c()),length(ids))
+	names(unique.drugs) <- ids
+	#parameters
+	d.cols <- ncol(id.drugs)
+	#collect drugs from each row
+	for (i in 1:nrow(id.drugs)) {
+		x <- id.drugs[i,]
+		idx <- x$NACCI
+		unique.drugs[[idx]] <- c(unique.drugs[[idx]],unlist(c(unname(x[2:d.cols]))))
+		#get rid of repeats and blanks
+		unique.drugs[[idx]] <- unique(unique.drugs[[idx]][unique.drugs[[idx]] != ""])
+	}
+	#flatten list and make table
+	unique.drugs.table <- sort(table(unlist(unname(unique.drugs))))
+	#print most and least common
+	tail(unique.drugs.table)
+	head(unique.drugs.table)
+	#output to csv
+	write.table(unique.drugs.table,file="sorted_unique_drugs_table.csv",sep=",",col.names=NA,qmethod="double")
 }
+
