283a82668aab41dfaecea0fa82d5cd3db93345cf
Sasi Madugula <madsas.sasi@gmail.com>
2014-12-25 19:58:56 -0800

    added precalculated files

--- drug_analysis_edited.R
+++ drug_analysis_edited.R
@@ -5,13 +5,8 @@ require(survival)
 data <- read.csv("altmann06122013.csv", as.is=T)
 data.df <- data.frame(data)
 
-message("preparing table... (takes a while)")
-xtab <- t(sapply(ids,function(x){ getInfo(data,x)}))
-colnames(xtab) <- cnames
-save(xtab, file=xtab.fname)
-
 #prepared data
-xtab.fname <- "xxx.RData"
+xtab.fname <- "drug_conversion.RData"
 #xtab.fname <- "/mnt/mapricot/musk2/home/altmann/data/NACC/RData/drug_conversion.RData"
 
 #Read in each of the drugs#
@@ -20,7 +15,8 @@ drug_list <- drug_list$Drug
 drug_list <- as.character(drug_list)
 drug_list <- t(drug_list)
 drug_list <- as.vector(drug_list)
-drug_list <- drug_list[1489:1584]
+#drug_list <- drug_list[1489:1584]
+drug_list <- drug_list[1489]
 
 for (cur_drug in drug_list) {
 
@@ -241,10 +237,10 @@ mydata <- data.frame(cbind(xtab, drugcol, drugdur))
 
 ##select subset to work with
 #HC @ baseline
-mydata.use <- subset(mydata, subset=DX==1 & !is.na(APOE))
+#mydata.use <- subset(mydata, subset=DX==1 & !is.na(APOE))
 
 #MCI @ baseline
-mydata.use <- subset(mydata, subset=DX==2 & !is.na(APOE))
+#mydata.use <- subset(mydata, subset=DX==2 & !is.na(APOE))
 
 #HC or MCI @ baseline
 mydata.use <- subset(mydata, subset=(DX==1 | DX==2) & !is.na(APOE))
@@ -276,34 +272,8 @@ convert    <- apply(mydata.use[,c("CONVMCI","CONVAD")],1,max)
 
 mysurv <- Surv(agestart, ageconvert, convert)
 
-######Add num_drugs
-ppp <- t(sapply(ids, function(y) {
-    idx <- data$NACCID == y
-    ttt <- data[idx,]
-    first_row <- ttt[1,]
-    drugs <- first_row[c(26:65)]
-    num_drugs <- 0
-    for (i in 1:40) {
-    drug <- drugs[i]
-       if (drug != "") {
-          num_drugs <- num_drugs + 1
-       }
-    }
-    return (c(y, num_drugs))
-}))
-
-colnames(ppp) <- c("NACCID", "NUMDRUGS")
-
-#NUMDRUGS <- ppp[,2]
-#NUMDRUGS <- as.numeric(NUMDRUGS)
-NUMDRUGS <- c()
-for (row_name in rownames(mydata.use)) {
-    row <- grep(row_name, rownames(ppp))
-    if (length(row) != 0) {
-        n_d <- as.numeric(ppp[row,2])
-        NUMDRUGS <- c(NUMDRUGS, n_d)
-    }
-}
+#LOAD NUMDRUGS--SASI
+load("NUMDRUGS.Rdata")
 
 #all subjects 
 coxph_val <- coxph(mysurv ~ DOXAZOSIN_dur + NUMDRUGS + MMSE + EDUX + APOE2 + SEX * APOE4, data=mydata.use, subset = AGE >= 55)
